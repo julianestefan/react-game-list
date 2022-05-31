ab321f5b2f6484a1303c9258d8d01e6a2e97aa14
Julian Estefan <julian.estefan@hotovo.com>
2022-05-31 11:33:32 -0300

    *Added footer component * Added mui icons *created theme to override mui default style for buttons

--- src/components/Layout/Footer/index.tsx
+++ src/components/Layout/Footer/index.tsx
@@ -1,6 +1,16 @@
+import { Button, Grid, IconButton } from "@mui/material";
+import DensityMediumIcon from '@mui/icons-material/DensityMedium';
+
+import styles from './styles.module.css'
+
 function Footer() {
-    return <footer role="footer" >Footer</footer>;
-  }
-  
-  export default Footer;
-  
+  return (
+    <footer className={styles.footer} role="footer">
+      <Button className={styles.actionButton} variant="contained" color="primary">
+        <DensityMediumIcon />
+      </Button>
+    </footer>
+  );
+}
+
+export default Footer;
