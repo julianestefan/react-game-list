797c161bd3ac2ba7a268e183832163f3b1613915
Julian Estefan <julian.estefan@hotovo.com>
2022-05-31 10:34:24 -0300

    Finished layout and navbar

--- src/components/Layout/Footer/index.tsx
+++ src/components/Layout/Footer/index.tsx
@@ -1,5 +1,5 @@
 function Footer() {
-    return <div>Footer</div>;
+    return <footer role="footer" >Footer</footer>;
   }
   
   export default Footer;
--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -3,16 +3,15 @@ import Navbar from "./Navbar";
 import Footer from "./Footer";
 import { Grid } from "@mui/material";
 
-
 interface LayoutProps {
   children: ReactElement;
 }
 
 function Layout({ children }: LayoutProps) {
   return (
-    <Grid container role="layout" >
+    <Grid container role="layout">
       <Navbar />
-      {children}
+      <main>{children}</main>
       <Footer />
     </Grid>
   );
