44d14143fafc57f2b050572eca195879139da826
Julian Estefan <julian.estefan@hotovo.com>
2022-05-31 11:38:39 -0300

    Added game list item component

--- src/components/GameList/GameItem/index.tsx
+++ src/components/GameList/GameItem/index.tsx
@@ -1,10 +1,19 @@
-interface GameItemProps{
+import { Grid, Typography } from "@mui/material";
+
+import styles from "./styles.module.css";
+
+interface GameItemProps {
   name: string;
   imageUrl: string;
 }
 
-function GameItem({name, imageUrl} : GameItemProps) {
-  return <div>GameItem</div>;
+function GameItem({ name, imageUrl }: GameItemProps) {
+  return (
+    <Grid item xs={6} md={3}>
+      <img className={styles.image} src={imageUrl} alt={name} />
+      <Typography variant="h3"> {name} </Typography>
+    </Grid>
+  );
 }
 
 export default GameItem;
